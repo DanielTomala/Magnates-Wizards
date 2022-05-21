03d0a9af66d11d9686775da70318f76d5e90f5f0
iokninsk <01158211@pw.edu.pl>
2022-05-22 01:45:56 +0200

    add button class

--- headers/button.hpp
+++ headers/button.hpp
@@ -27,14 +27,21 @@ class Button{
     
     public:
         Button(float topLeftX, float topLeftY, float width, float height,
-        std::shared_ptr<sf::Font> font,
+        std::shared_ptr<sf::Font> font, std::string text,
         sf::Color normalColor, sf::Color highlightedColor, sf::Color clickedColor,
         unsigned int id);
 
         ~Button();
 
+        bool isClicked() const;
+        std::string getText() const;
+        unsigned int getId() const;
 
+        void setText(const std::string &text);
+        void setId(const unsigned int &id);
 
+        void update(const sf::Vector2i& mousePosition);
+        void render(std::shared_ptr<sf::RenderTarget> window);
 };
 
 #endif
---
+++ src/button.cpp
@@ -0,0 +1,86 @@
+#include "../headers/button.hpp"
+
+Button::Button(float topLeftX, float topLeftY, float width, float height,
+        std::shared_ptr<sf::Font> font, std::string text,
+        sf::Color normalColor, sf::Color highlightedColor, sf::Color clickedColor,
+        unsigned int id)
+        {
+            this->id = id;
+            this->state = normal;
+
+            this->rect.setPosition(sf::Vector2f(topLeftX, topLeftY));
+            this->rect.setSize(sf::Vector2f(width, height));
+            this->rect.setFillColor(normalColor);
+            this->rect.setOutlineColor(sf::Color::Black);
+            this->rect.setOutlineThickness(1.f);
+
+            this->font = font;
+            this->text.setFont(*this->font);
+            this->text.setString(text);
+            this->text.setFillColor(sf::Color::Black);
+            this->text.setCharacterSize(14);
+
+            this->text.setPosition(
+                this->rect.getPosition().x + (this->rect.getGlobalBounds().width / 2.f) - this->text.getGlobalBounds().width / 2.f,
+		        this->rect.getPosition().y
+            );
+
+            this->highlightedColor = highlightedColor;
+            this->clickedColor = clickedColor;
+        }
+
+Button::~Button() {}
+
+bool Button::isClicked() const{
+    if(this->state == clicked){
+        return true;
+    }
+    return false;
+}
+
+std::string Button::getText() const{
+    return this->text.getString();
+}
+
+unsigned int Button::getId() const{
+    return this->id;
+}
+
+void Button::setText(const std::string &text){
+    this->text.setString(text);
+}
+
+void Button::setId(const unsigned int id){
+    this->id = id;
+}
+
+void Button::update(const sf::Vector2i& mousePosition){
+
+    this->state = normal;
+
+    if(this->rect.getGlobalBounds().contains(sf::Vector2f(mousePosition))){
+        this->state = highlighted;
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            this->state = clicked;
+        }
+    }
+
+    switch(this->state){
+        case normal:
+            this->rect.setFillColor(this->normalColor);
+            break;
+        case highlighted:
+            this->rect.setFillColor(this->highlightedColor);
+            break;
+        case clicked:
+            this->rect.setFillColor(this->clickedColor);
+            break;
+    }
+}
+
+void Button::render(std::shared_ptr<sf::RenderTarget> window){
+    window->draw(this->rect);
+    window->draw(this->text);
+}
+
