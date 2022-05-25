110c4af4c0371342b782e057b53c9b8b990dea76
dtomala <01159155@pw.edu.pl>
2022-05-25 23:19:10 +0200

    Added few simple descriptions

--- src/ui/descriptions.cpp
+++ src/ui/descriptions.cpp
@@ -6,16 +6,16 @@
 Descriptions::Descriptions(){
     this->heroesDescriptions[EArcher] = "Archer has a bow.\nAttack range: 1 - 3\nMove range: 1\nDamage: 10 - 50\n";
     this->heroesDescriptions[EKnight] = "Knight has sword.\nAttack range: 1\nMove range: 1\nDamage: 5 - 20\n";
-    this->heroesDescriptions[EWizard] = "";
-    this->heroesDescriptions[EIceDruid] = "";
-    this->heroesDescriptions[EMedic] = "";
-    this->heroesDescriptions[ECatapult] ="";
-    this->heroesDescriptions[ENinja] ="";
-    this->heroesDescriptions[ETrebuchet] ="";
+    this->heroesDescriptions[EWizard] = "Wizard";
+    this->heroesDescriptions[EIceDruid] = "Ice Druid";
+    this->heroesDescriptions[EMedic] = "Medic";
+    this->heroesDescriptions[ECatapult] ="Catapult";
+    this->heroesDescriptions[ENinja] ="Ninja";
+    this->heroesDescriptions[ETrebuchet] ="Trebuchet";
 }
 
 Descriptions::~Descriptions(){}
-        
+
 std::string Descriptions::getDescription(const HeroType &heroType) const{
     return this->heroesDescriptions.at(heroType);
 }
