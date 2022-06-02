5fd1ea77a1069a2ed167642117317b03efea6b8e
dtomala <01159155@pw.edu.pl>
2022-06-02 13:33:18 +0200

    Default action works properly

--- headers/game_logic/weapons/bow.hpp
+++ headers/game_logic/weapons/bow.hpp
@@ -5,15 +5,6 @@
 
 class Bow : public Weapon
 {
-protected:
-    unsigned int range;
-    unsigned int damage;
-
-public:
-    unsigned int getRange() const;
-    void setRange(unsigned int range);
-    unsigned int getDamage() const;
-    void setDamage(unsigned int damage);
 };
 
 #endif
--- headers/game_logic/weapons/stone.hpp
+++ headers/game_logic/weapons/stone.hpp
@@ -5,14 +5,6 @@
 
 class Stone : public Weapon
 {
-protected:
-    unsigned int range;
-    unsigned int damage;
-public:
-    unsigned int getRange() const;
-    void setRange(unsigned int range);
-    unsigned int getDamage() const;
-    void setDamage(unsigned int damage);
 };
 
 #endif
